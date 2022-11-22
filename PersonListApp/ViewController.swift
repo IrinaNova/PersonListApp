691d37b7422034ce050e5e7875a56d9bf2df8f9c
IrinaNova <irishaivashenok@mail.ru>
2022-11-22 21:13:55 +0300

    Initial Commit

---
+++ PersonListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonListApp
+//
+//  Created by Irunchik on 22.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
